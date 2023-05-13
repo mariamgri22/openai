ad13237ff83341bb436a2b38d9e89ef32c165cd1
mariamgri22 <mariamgrigoryan99@gmail.com>
2023-05-13 19:54:41 +0400

    feat:add html

--- src/routes/openaiRoute.js
+++ src/routes/openaiRoute.js
@@ -1,10 +1,26 @@
 const express = require("express");
-const {
-  generateOpenAIResponse,
-} = require("../controllers/openaiController");
+const { generateOpenAIResponse } = require("../controllers/openaiController");
 
 const router = express.Router();
 
-router.get("/", generateOpenAIResponse);
+router.get("/", (req, res) => {
+  res.send(`
+      <html>
+        <head>
+          <title>OpenAI Chat</title>
+        </head>
+        <body>
+          <h1>OpenAI Chat</h1>
+          <form method="GET" action="/openai">
+            <label for="message">Message:</label>
+            <input type="text" name="message" id="message" required>
+            <button type="submit">Send</button>
+          </form>
+        </body>
+      </html>
+    `);
+});
+
+router.get("/openai", generateOpenAIResponse);
 
 module.exports = { router };
