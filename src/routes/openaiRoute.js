ba677c3ed8feb9b204528e1529f0ff8d89e0d3d0
mariamgri22 <mariamgrigoryan99@gmail.com>
2023-05-18 16:59:25 +0400

    fix:change post request

--- src/routes/openaiRoute.js
+++ src/routes/openaiRoute.js
@@ -3,24 +3,6 @@ const { generateOpenAIResponse } = require("../controllers/openaiController");
 
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.send(`
-      <html>
-        <head>
-          <title>OpenAI Chat</title>
-        </head>
-        <body>
-          <h1>OpenAI Chat</h1>
-          <form method="GET" action="/openai">
-            <label for="message">Message:</label>
-            <input type="text" name="message" id="message" required>
-            <button type="submit">Send</button>
-          </form>
-        </body>
-      </html>
-    `);
-});
-
-router.get("/openai", generateOpenAIResponse);
+router.post("/openai", generateOpenAIResponse);
 
 module.exports = { router };
